e28cb40e248814cc551f8580ee55a5a9bb2a55af
BrandonLiang <brandonliang@qq.com>
2021-01-04 23:02:12 -0500

    conversion and now training on VCR data

--- python/annotation_to_DPR_data.py
+++ python/annotation_to_DPR_data.py
@@ -2,6 +2,7 @@ import argparse
 import pandas as pd
 import os
 import json
+import random
 
 def convert(args):
   if not os.path.isfile(args.tsv_output):
@@ -14,29 +15,54 @@ def convert(args):
     df[["passage_id", "title", "text"]].to_csv(args.tsv_output, sep = '\t', index = False)
 
   indexed_df = pd.read_csv(args.tsv_output, delimiter = '\t')
-  # https://hackersandslackers.com/multiple-aggregations-pandas/
-  indexed_df = indexed_df.astype(str).groupby("title", sort = False).agg(unique_passage_id = ("passage_id", "unique"), unique_text = ("text", "unique"))
+  # https://stackoverflow.com/a/58435858
+  indexed_df = indexed_df.astype(str).groupby("title", sort = False).agg(unique_passage_id = ("passage_id", "unique"), unique_text = ("text", "unique")).reset_index()
   DATASET = "VCR"
   data = []
+  size = indexed_df.shape[0]
+  #print(indexed_df.head())
   for index, row in indexed_df.iterrows():
+    title = row["title"]
     entry = {}
     entry["dataset"] = DATASET
-    entry["question"] = row.index
+    entry["question"] = title
     entry["answers"] = [] # leave blank for now, may add later
     positive_ctxs = []
     for passage_id, text in zip(row["unique_passage_id"], row["unique_text"]):
       positive_ctxs.append({
-        "title": row.index,
+        "title": title,
         "text": text,
         "score": 1000, # may change later
         "title_score": 1,
         "passage_id": passage_id
       })
     entry["positive_ctxs"] = positive_ctxs
+
     # negative_ctxs
+    negative_ctxs = []
+    negative_index = random.randint(0, size - 1)
+    while negative_index == index:
+      negative_index = random.randint(0, size - 1)
+    negative_row = indexed_df.iloc[negative_index]
+    negative_title = negative_row["title"]
+    for passage_id, text in zip(negative_row["unique_passage_id"], row["unique_text"]):
+      negative_ctxs.append({
+        "title": negative_title,
+        "text": text,
+        "score": 0,
+        "title_score": 0,
+        "passage_id": passage_id
+      })
+    entry["negative_ctxs"] = negative_ctxs
+
+    # hard_negative_ctxs
+    hard_negative_ctxs = []
+    entry["hard_negative_ctxs"] = hard_negative_ctxs
+
     data.append(entry)
 
-  with open(args.dpr_intput, 'w') as f:
+  #data = json.dumps(data)
+  with open(args.dpr_input, 'w') as f:
     json.dump(data, f, indent = 4)
 
 
--- run.sh
+++ run.sh
@@ -15,8 +15,10 @@ source "$ENV"
 
 DATA_DIR=$APP_HOME/data/vcr
 mkdir -p $DATA_DIR
+CHECKPOINT_DIR=$APP_HOME/checkpoint
+mkdir -p $CHECKPOINT_DIR/vcr
 
-STAGE=1
+STAGE=2
 
 if [ $STAGE -le 1 ]; then
   python $PYTHON_DIR/annotation_to_DPR_data.py \
@@ -26,5 +28,24 @@ if [ $STAGE -le 1 ]; then
     --dpr_input $DATA_DIR/train.json
 fi
 
-#if [ $STAGE -le 2 ]; then
-#fi
+if [ $STAGE -le 2 ]; then
+  python -m torch.distributed.launch \
+    --nproc_per_node=4 train_dense_encoder.py \
+    --max_grad_norm 2.0 \
+    --encoder_model_type hf_bert \
+    --pretrained_model_cfg bert-base-uncased \
+    --seed 12345 \
+    --sequence_length 64 \
+    --warmup_steps 1237 \
+    --batch_size 32 \
+    --do_lower_case \
+    --train_file ${DATA_DIR}/train.json \
+    --dev_file ${DATA_DIR}/train.json \
+    --output_dir $CHECKPOINT_DIR/vcr \
+    --learning_rate 2e-5 \
+    --num_train_epochs 40 \
+    --dev_batch_size $BATCH_SIZE \
+    --val_av_rank_start_epoch 30 \
+    --global_loss_buf_sz 1500000
+fi
+
