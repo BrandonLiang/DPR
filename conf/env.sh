c19794de92a5bb04290edf302f76156d1cddcffa
BrandonLiang <brandonliang@qq.com>
2021-01-27 19:22:15 -0500

    add dense_retriever_using_Query_Encoder_only.py to generate updated retrieval documents using updated Query Encoder only (rather than the entire DPR biencoder model which was required by dense_retriever.py) so that it is easier to generate updated retrieval documents after epoch of Visual-Comet fine-tuning using DPR Query Encoder

--- conf/env.sh
+++ conf/env.sh
@@ -1,5 +1,7 @@
+USER=xudong
+HOME=/home/$USER
 TSV_DIR=$HOME/src/mycode/clustering/exact_match/cluster_result
 TRANSFORMERS_DIR=$HOME/src/transformers
-RETRIEVAL_DIR=$HOME/src/visual-comet/experiments/image-inference-80000-ckpt
+RETRIEVAL_DIR=$HOME/src/visual-comet/retrieval_documents/
 
 BATCH_SIZE=8
